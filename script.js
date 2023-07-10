5699039cc645d642279d8b34ec6816c95ca6e4d9
Amila Abdulović <116310565+AmilaAbdulovic@users.noreply.github.com>
2023-07-10 09:00:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 $(document).ready(function() {
 
   document.getElementById('submitBtn').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
   
-    // Show the overlay and pop-up
+  
     document.getElementById('overlay').style.display = 'block';
   });
   
   document.getElementById('closeBtn').addEventListener('click', function() {
-    // Hide the overlay and pop-up
+   
     document.getElementById('overlay').style.display = 'none';
   
   });
@@ -23,7 +23,6 @@ $(document).ready(function() {
       type: 'GET',
       dataType: 'html',
       success: function(response) {
-        // Update the content of the <body> element with the loaded page
         $('body').html(response);
       },
       error: function(xhr, status, error) {
